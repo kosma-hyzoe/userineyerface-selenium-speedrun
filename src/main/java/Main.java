7c3b03cf80037f0022c0393772408534c796ed46
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-01-31 22:53:00 +0100

    basic project structure added

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.browser.Browser;
+
+public class Main {
+    private static final Browser browser = AqualityServices.getBrowser();
+    public static void main(String[] args) {
+
+    }
+}
